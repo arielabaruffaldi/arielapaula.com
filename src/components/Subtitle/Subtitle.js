71dc37ef65987e22fc3b991e1e4fe896d5fb7a37
ariela <baruffaldiariela@gmail.com>
2020-10-26 20:47:07 -0300

    ajuste estilos subtitulos

--- src/components/Subtitle/Subtitle.js
+++ src/components/Subtitle/Subtitle.js
@@ -1,10 +1,10 @@
 import React from "react";
 import styles from "./Subtitle.module.scss"
 
-const Subtitle = ({ text, secondaryText, etiqueta }) => {
+const Subtitle = ({ text, secondaryText, etiqueta, separator }) => {
     return (
         etiqueta === "h1" ? <h1 className={styles.Subtitle}>{text}<div className={styles.separator}></div>{secondaryText && <span>{secondaryText}</span>}</h1> :
-            <h2 className={styles.Subtitle}>{text}<div className={styles.separator}></div><span>{secondaryText}</span></h2>
+            <h2 className={styles.Subtitle}>{text}{separator && <div className={styles.separator}></div>}<span>{secondaryText}</span></h2>
     )
 }
 
