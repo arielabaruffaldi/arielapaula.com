e43ec3af5f496244f88f3100bf0cb7ddde8da31a
Tomas <angelotti.tomas@gmail.com>
2021-03-22 16:51:34 -0300

    Ajusto form

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,14 +2,15 @@ import React from 'react';
 import styles from "./Button.module.scss"
 import PropTypes from "prop-types";
 
-const Button = ({ href, text, color = "white", children, classes, onClick, size = 1, weight = 300, tag = "button", ...props }) => {
+const Button = ({ type, href, text, color = "white", children, classes, onClick, size = 1, weight = 300, tag = "button", ...props }) => {
     let CustomTag = tag;
     return (
         <CustomTag
-            to={`${href}`}
+            to={`${href ? href : ""}`}
             className={
                 `${styles.Button} ${classes ? classes : ""} ${styles[color]}`
             }
+            type={type ? type : ""}
             onClick={onClick}
             {...props}
         >
